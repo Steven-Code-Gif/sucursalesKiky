60465e666da31e2cde8b8d022599c7860cd88f39
Steven Escobar <84638963+Steven-Code-Gif@users.noreply.github.com>
2023-01-09 22:04:56 -0400

    sucursales

---
+++ assets/js/maps.js
@@ -0,0 +1,212 @@
+//Texto Marcador
+        const marker_cusis =
+        "<h2>Kiky Los Cusis</h2>" +
+        "<p> Av. Los Cusis </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=59176327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/aB9JRJ7buRSSbryi9">Ver en Google Maps</a></p>'
+        ;
+        const marker_santacruz =
+        "<h2>Kiky Santa Cruz</h2>" +
+        "<p> Av. Santa Cruz 2do anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=59176327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/BbnQEawzkn9NWiTq8">Ver en Google Maps</a></p>'
+        ;
+        const marker_ventura =
+        "<h2>Kiky Ventura</h2>" +
+        "<p> Av. 4to anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=59176327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/b4tvtKpofcU4Tk4V9">Ver en Google Maps</a></p>'
+        ;   
+        const marker_grigota =
+        "<h2>Kiky Grigota</h2>" +
+        "<p>Av. Doble vía la guardia 5to anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=59176327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/JE9DaNQStEe999fj6">Ver en Google Maps</a></p>'
+        ;   
+        const marker_banzer =
+        "<h2>Kiky Banzer</h2>" +
+        "<p>Av. Banzer Entre 7mo y 8vo anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=591776327635text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/aHdrq6ZK7Eyi1n2N8">Ver en Google Maps</a></p>'
+        ;   
+        const marker_virgencotoca =
+        "<h2>Kiky Virgen cotoca</h2>" +
+        "<p>Av. 4to anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=59176327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/ktxEitVx5NNFjc9z7">Ver en Google Maps</a></p>'
+        ;   
+        const marker_trompillo =
+        "<h2>Kiky Trompillo</h2>" +
+        "<p>Av. El trompillo 2do anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/YMUnTJMd4cJnW6wb6">Ver en Google Maps</a></p>'
+        ;   
+        const marker_pirai =
+        "<h2>Kiky Pirai</h2>" +
+        "<p> Av. Pirai Entre 2do y 3er anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/AzMCLQaCNuYjYhnYA">Ver en Google Maps</a></p>'
+        const marker_upsa =
+        "<h2>Kiky Upsa</h2>" +
+        "<p> Av. Paragua 4to anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/qjSSzpoxee3gKiDt6">Ver en Google Maps</a></p>'
+        ;   
+        const marker_santosdumont =
+        "<h2>Kiky Santos Dumont</h2>" +
+        "<p> Av. Santos Dumont Entre 3er y 4to anillo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/VCk263TybkskqvE1A">Ver en Google Maps</a></p>'
+        ;   
+        const marker_bush =
+        "<h2>Kiky Bush</h2>" +
+        "<p> Av. 3er anillo interno </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/wkzUiW71VXFCS2Pj8">Ver en Google Maps</a></p>'
+        ;   
+        const marker_fidalga =
+        "<h2>Kiky Fidalga</h2>" +
+        "<p> Av. Cristo Redentor 3er anillo externo </p>"+
+        "<p><a target='_blank' href='https://api.whatsapp.com/send/?phone=76327635&text&type=phone_number&app_absent=0'><img src='/assets/img/whatsapp.svg'> 721 07 107</a></p>" +
+        '<p><a target="_blank" href="https://goo.gl/maps/M8DLtwqQZ78kLq8L8">Ver en Google Maps</a></p>'
+        ;   
+        var routes = document.getElementById("route-btn")
+        var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a style="color:#CE111B ;" href="https://github.com/Steven-Code-Gif" title="Code by NRS">SEC</a> contributors'
+        });
+		osmhumanitarian = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org">' +
+			'Colaboradores de OpenStreetMap</a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas \u00a9 <a href="https://github.com/hotosm/HDM-CartoCSS">Humanitarian HDM CartoCSS</a> (<a href="https://github.com/hotosm/HDM-CartoCSS/blob/master/LICENCE.txt" target="blanck">CC0 1.0</a>)'
+      });
+  var map = L.map('map', {
+  center: [-17.7820725,-63.1702058],
+  zoom: 14,
+  layers: [osmhumanitarian],
+  gestureHandling: true
+});
+var baseLayers = {
+  "Callejero (OSM)": osm,
+  "Humanitario (OSM)": osmhumanitarian,
+};
+L.control.layers(baseLayers).addTo(map);
+// IMAGEN
+var LeafIcon = L.Icon.extend({
+  options: {
+    iconSize:     [58, 95],
+    shadowSize:   [50, 64],
+    iconAnchor:   [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor:  [-3, -76]
+  }
+});
+//Agregar variable de nueva sucursal
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 23.svg'});
+var succusis = L.marker([-17.7639145, -63.1766372], {icon: icon,title: 'Kiky Cusis',riseOnHover:true,riseOffset:250}).bindPopup(marker_cusis).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 18.svg'});
+var sucsantacruz = L.marker([-17.7808781, -63.164418], {icon: icon,title: 'Kiky Santa Cruz',riseOnHover:true,riseOffset:250}).bindPopup(marker_santacruz).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 20.svg'});
+var sucventura = L.marker([-17.7537196, -63.1954736], {icon: icon,title: 'Kiky Ventura',riseOnHover:true,riseOffset:250}).bindPopup(marker_ventura).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 21.svg'});
+var sucgrigota = L.marker([-17.815788, -63.2131416], {icon: icon,title: 'Kiky Grigota',riseOnHover:true,riseOffset:250}).bindPopup(marker_grigota).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 19.svg'});
+var sucbanzer = L.marker([-17.7207517, -63.1634934], {icon: icon,title: 'Kiky Banzer',riseOnHover:true,riseOffset:250}).bindPopup(marker_banzer).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 13.svg'});
+var sucvirgencotoca = L.marker([-17.7824074, -63.1480833], {icon: icon,title: 'Kiky Virgen Cotoca',riseOnHover:true,riseOffset:250}).bindPopup(marker_virgencotoca).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 14.svg'});
+var suctrompillo = L.marker([-17.7991479, -63.1759888], {icon: icon,title: 'Kiky Trompillo',riseOnHover:true,riseOffset:250}).bindPopup(marker_trompillo).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 15.svg'});
+var sucpirai = L.marker([-17.7931562, -63.2010791], {icon: icon,title: 'Kiky Pirai',riseOnHover:true,riseOffset:250}).bindPopup(marker_pirai).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 16.svg'});
+var sucupsa = L.marker([-17.7641004, -63.1477008], {icon: icon,title: 'Kiky Upsa',riseOnHover:true,riseOffset:250}).bindPopup(marker_upsa).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 22.svg'});
+var sucsantosdumont = L.marker([-17.8213161, -63.1843241], {icon: icon,title: 'Kiky Santos Dumont',riseOnHover:true,riseOffset:250}).bindPopup(marker_santosdumont).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 24.svg'});
+var sucbush = L.marker([-17.7707817, -63.2023941], {icon: icon,title: 'Kiky Bush',riseOnHover:true,riseOffset:250}).bindPopup(marker_bush).addTo(map);
+var icon = new LeafIcon({iconUrl: '/assets/img/iconos/Recurso 17.svg'});
+var sucfidalga = L.marker([-17.7583528, -63.1781942], {icon: icon,title: 'Kiky Fidalga',riseOnHover:true,riseOffset:250}).bindPopup(marker_fidalga).addTo(map);
+var locateMe = L.control.locate({
+  locateOptions:{
+     maxZoom: 16,
+     enableHighAccuracy: true
+  },
+  strings:{
+      title: "Activar mi Ubicación",
+      popup: "Estas cerca de {distance} metros desde aquí",
+  },
+}).addTo(map);
+
+    routes.addEventListener("click", function () {
+    function recursivo() {
+      function locateTouch(me, to) {
+        var distance = map.distance(me,to);
+        return distance;
+    };
+    //Add de variables de Rutas
+      var d1 = locateTouch((locateMe._marker._latlng),(succusis._latlng))
+      var d2 = locateTouch((locateMe._marker._latlng),(sucsantacruz._latlng))
+      var d3 = locateTouch((locateMe._marker._latlng),(sucventura._latlng))
+      var d4 = locateTouch((locateMe._marker._latlng),(sucgrigota._latlng))
+      var d5 = locateTouch((locateMe._marker._latlng),(sucbanzer._latlng))
+      var d6 = locateTouch((locateMe._marker._latlng),(sucvirgencotoca._latlng))
+      var d7 = locateTouch((locateMe._marker._latlng),(suctrompillo._latlng))
+      var d8 = locateTouch((locateMe._marker._latlng),(sucpirai._latlng))
+      var d9 = locateTouch((locateMe._marker._latlng),(sucupsa._latlng))
+      var d10 = locateTouch((locateMe._marker._latlng),(sucsantosdumont._latlng))
+      var d11 = locateTouch((locateMe._marker._latlng),(sucbush._latlng))
+      var d12 = locateTouch((locateMe._marker._latlng),(sucfidalga._latlng))
+      //Agregar variables
+      var r = Math.min(...[d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12])
+      //agregar variables else if
+      if (r==d1) {
+        r = succusis._latlng
+      } else if (r==d2) {
+        r = sucsantacruz._latlng
+      }
+      else if (r==d3) {
+        r = sucventura._latlng
+      }
+      else if (r==d4) {
+        r = sucgrigota._latlng
+      }
+      else if (r==d5) {
+        r = sucbanzer._latlng
+      }
+      else if (r==d6) {
+        r = sucvirgencotoca._latlng
+      }
+      else if (r==d7) {
+        r = suctrompillo._latlng
+      }
+      else if (r==d8) {
+        r = sucpirai._latlng
+      }
+      else if (r==d9) {
+        r = sucupsa._latlng
+      }
+      else if (r==d10) {
+        r = sucsantosdumont._latlng
+      }
+      else if (r==d11) {
+        r = sucbush._latlng
+      }
+      else if (r==d12) {
+        r = sucfidalga._latlng
+      }
+      return r;
+  }
+    var routing = new L.Routing.control({
+      addWaypoints:false,
+      draggableWaypoints:false,
+      routeWhileDragging:[true],
+      language: 'es', 
+      waypoints: [
+          L.latLng(locateMe._marker._latlng),
+          L.latLng(recursivo())
+      ],
+    }).addTo(map);
+    if (locateMe._active===true) {
+      routes.classList.add("active");
+    }
+  });
+
